87e1d5c5a3e9605209de0b180bcf13ec7bc96f3a
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2026-05-16 13:33:55 +0200

    little modify in .gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,6 @@ import './App.css';
 import DefaultLayout from './layouts/DefaultLayout';
 import Homepage from './pages/Homepage';
 
-const BASE_STORAGE_URL = `${import.meta.env.VITE_SUPABASE_URL}/storage/v1/object/public/`;
-
-console.log(BASE_STORAGE_URL);
 
 export default function App() {
 
